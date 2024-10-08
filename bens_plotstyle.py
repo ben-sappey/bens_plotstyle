9705f4023cb7d28ebb1a4e1e26989a653e8b9737
ben-sappey <bsappey@ucsd.edu>
2024-10-08 08:50:55 -0700

    Update bens_plotstyle.py

--- bens_plotstyle.py
+++ bens_plotstyle.py
@@ -2,17 +2,22 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 def main():
-    font_dirs = ["/Users/bensappey/Library/Fonts/"]  # The path to the custom font file.
+    font_dirs = ["~/Library/Fonts/"]  # The path to the custom font file. Modify if your font library isn't here
     font_files = mpl.font_manager.findSystemFonts(fontpaths=font_dirs)
 
     for font_file in font_files:
         mpl.font_manager.fontManager.addfont(font_file) 
-    mpl.font_manager.findfont('JuliaMono')
-    plt.style.use('/Users/bensappey/random/pretty_plotting_default.mplstyle')
+    try:    
+        mpl.font_manager.findfont('JuliaMono')
+        print("Found font: JuliaMono")
+    except:
+        print("Font 'JuliaMono' not found")
+        
+    plt.style.use('./pretty_plotting_default.mplstyle')
    
 
 def paper():
-    plt.style.use('/Users/bensappey/random/pretty_plotting_paper.mplstyle')
+    plt.style.use('./pretty_plotting_paper.mplstyle')
 
 if __name__ == '__main__':
     main()
